9049f85cac93faf972c41c77038c96a5d51c0f59
Emir Akyüz <emirakyuz81@gmail.com>
2023-12-06 03:45:51 +0300

    # Update <label> issue test

--- components/LanguageDropdown.tsx
+++ components/LanguageDropdown.tsx
@@ -8,9 +8,9 @@ const LanguageDropdown = ({ width }: any | undefined) => {
         i18n.changeLanguage(selectedLanguage)
     };
     return <>
-        <select className={`p-1 border text-black bg-transparent dark:text-white ${width ? width : ""}`} value={i18n.language} onChange={toggleLanguage}>
-            <option className={`text-black dark:text-white dark:bg-black ${i18n.language === 'tr' ? 'font-bold' : ''}`} disabled={i18n.language === 'tr' ? true : false} value={'tr'}>TR</option>
-            <option className={`text-black dark:text-white dark:bg-black ${i18n.language === 'en' ? 'font-bold' : ''}`} disabled={i18n.language === 'en' ? true : false} value={'en'}>EN</option>
+        <select aria-labelledby="language" className={`p-1 border text-black bg-transparent dark:text-white ${width ? width : ""}`} value={i18n.language} onChange={toggleLanguage}>
+            <option id="language" aria-label="language" className={`text-black dark:text-white dark:bg-black ${i18n.language === 'tr' ? 'font-bold' : ''}`} disabled={i18n.language === 'tr' ? true : false} value={'tr'}>TR</option>
+            <option id="language" aria-label="language" className={`text-black dark:text-white dark:bg-black ${i18n.language === 'en' ? 'font-bold' : ''}`} disabled={i18n.language === 'en' ? true : false} value={'en'}>EN</option>
         </select>
     </>
 }
